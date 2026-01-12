a7882832c7901137bec154e729b7d30142ad6678
Mergim Januzi <mergim.januzi@gmail.com>
2026-01-12 12:49:05 +0100

    import changes on model to prevent start imports

--- src/main/java/healthcareab/project/healthcare_booking_app/models/User.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/models/User.java
@@ -1,6 +1,17 @@
 package healthcareab.project.healthcare_booking_app.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.FetchType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
