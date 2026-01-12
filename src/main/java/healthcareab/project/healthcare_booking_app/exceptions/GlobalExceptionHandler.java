77b28f6c4344e0e2234c0ed0a942572c020532d9
Mergim Januzi <mergim.januzi@gmail.com>
2026-01-12 10:29:35 +0100

    HandleGeneralExpetion added in globalhandleException for server errors

--- src/main/java/healthcareab/project/healthcare_booking_app/exceptions/GlobalExceptionHandler.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/exceptions/GlobalExceptionHandler.java
@@ -12,7 +12,10 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
-
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneral(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.");
+    }
 
 
 }
