3b33e7bc207fbafe01e81da208b8517d395c3c54
Mergim Januzi <mergim.januzi@gmail.com>
2026-01-12 10:27:33 +0100

    IllegalArgumentExpetion added in globalExceptionhandler for all BAD_REQUESTS responses

---
+++ src/main/java/healthcareab/project/healthcare_booking_app/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package healthcareab.project.healthcare_booking_app.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+
+
+
+}
