6a23313ded814d2b516d1a8d1f41927d32053934
Mergim Januzi <mergim.januzi@gmail.com>
2026-01-12 10:31:20 +0100

    added a dynamic exception for custom made expetions

--- src/main/java/healthcareab/project/healthcare_booking_app/exceptions/GlobalExceptionHandler.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/exceptions/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,6 +17,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleGeneral(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.");
     }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
+        return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason());
+    }
+
+
 
 
 }
