aaeb5410cd09fc000da9bf09c45da678d3f96e3b
Mergim Januzi <mergim.januzi@gmail.com>
2026-01-13 11:50:04 +0100

    Revert "RegisterDTO updated for employee and patient"
    This reverts commit 0f32e27610085472dbca4d9aeb29fd1e502039e4.

--- src/main/java/healthcareab/project/healthcare_booking_app/dto/RegisterRequest.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/dto/RegisterRequest.java
@@ -4,52 +4,33 @@ import healthcareab.project.healthcare_booking_app.models.Role;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
-import java.time.LocalDate;
 import java.util.Set;
 
 public class RegisterRequest {
     @NotBlank
     private String username;
-
     @NotBlank
     @Pattern(
-            regexp = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&()\\-_=+{};:,<.>]).{8,}$",
-            message = "Password must be at least 8 characters long and include at least " +
-                    "one uppercase letter, one number, and one special character."
+        regexp = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&()\\-_=+{};:,<.>]).{8,}$",
+        message = "Password must be at least 8 characters long and include at least " +
+                "one uppercase letter, one number, and one special character."
     )
     private String password;
-
     private Set<Role> roles;
+
     private String email;
     private String firstName;
     private String lastName;
 
-    private String phoneNumber;
-    private LocalDate dateOfBirth;
-
-    private String employeeNumber;
-    private String specialization;
-    private String department;
-
-    public RegisterRequest() {
-    }
-
-    public RegisterRequest(String username, String password, Set<Role> roles,
-                           String email, String firstName, String lastName,
-                           String phoneNumber, LocalDate dateOfBirth,
-                           String employeeNumber, String specialization, String department) {
+    public RegisterRequest(String username, String password, Set<Role> roles, String email, String firstName, String lastName) {
         this.username = username;
         this.password = password;
         this.roles = roles;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.dateOfBirth = dateOfBirth;
-        this.employeeNumber = employeeNumber;
-        this.specialization = specialization;
-        this.department = department;
     }
+
 
     public @NotBlank String getUsername() {
         return username;
@@ -73,69 +54,5 @@ public class RegisterRequest {
 
     public String getLastName() {
         return lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public String getEmployeeNumber() {
-        return employeeNumber;
-    }
-
-    public String getSpecialization() {
-        return specialization;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public void setEmployeeNumber(String employeeNumber) {
-        this.employeeNumber = employeeNumber;
-    }
-
-    public void setSpecialization(String specialization) {
-        this.specialization = specialization;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
     }
 }
