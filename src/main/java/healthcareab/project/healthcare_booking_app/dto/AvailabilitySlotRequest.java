42fc2242b3d5f4d0a5173ad74a67d5730385a874
Larsin15.Larsin15 <tommy.larsin@hotmail.com>
2026-01-14 12:42:15 +0100

    getters and setters and logging utility method

--- src/main/java/healthcareab/project/healthcare_booking_app/dto/AvailabilitySlotRequest.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/dto/AvailabilitySlotRequest.java
@@ -19,4 +19,28 @@ public class AvailabilitySlotRequest {
         this.startTime = startTime;
         this.endTime = endTime;
     }
+
+    public ZonedDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(ZonedDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public ZonedDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(ZonedDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return "AvailabilitySlotRequest{" + //representation for logging and debugging
+                "startTime=" + startTime +
+                ", endTime=" + endTime +
+                '}';
+    }
 }
