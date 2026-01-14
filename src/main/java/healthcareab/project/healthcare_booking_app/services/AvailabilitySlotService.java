0cf870e1f8d755342f7d2f7e8f9452160983d8c4
Mergim Januzi <mergim.januzi@gmail.com>
2026-01-14 14:12:50 +0100

    started service for availabilityslot with some constans with opening hours and contructor

---
+++ src/main/java/healthcareab/project/healthcare_booking_app/services/AvailabilitySlotService.java
@@ -0,0 +1,27 @@
+package healthcareab.project.healthcare_booking_app.services;
+
+import healthcareab.project.healthcare_booking_app.dto.AvailabilitySlotResponse;
+import healthcareab.project.healthcare_booking_app.repository.AvailabilitySlotRepository;
+import healthcareab.project.healthcare_booking_app.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AvailabilitySlotService {
+
+    private final AvailabilitySlotRepository availabilitySlotRepository;
+    private final EmployeeRepository employeeRepository;
+    private final AuthService authService;
+
+    // Business rules constants
+    private static final int WORKING_HOUR_START = 8;
+    private static final int WORKING_HOUR_END = 16;
+    private static final int SLOT_DURATION_MINUTES = 30;
+
+    public AvailabilitySlotService(AvailabilitySlotRepository availabilitySlotRepository, EmployeeRepository employeeRepository, AuthService authService) {
+        this.availabilitySlotRepository = availabilitySlotRepository;
+        this.employeeRepository = employeeRepository;
+        this.authService = authService;
+    }
+}
