413b88278f4eca393857ff2f9b4ad8a263aa3f4c
Larsin15.Larsin15 <tommy.larsin@hotmail.com>
2026-01-14 12:34:45 +0100

    private ZonedDateTime startTime, endTime

--- src/main/java/healthcareab/project/healthcare_booking_app/dto/AvailabilitySlotRequest.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/dto/AvailabilitySlotRequest.java
@@ -1,4 +1,15 @@
 package healthcareab.project.healthcare_booking_app.dto;
 
+import jakarta.validation.constraints.NotNull;
+
+import java.time.ZonedDateTime;
+
 public class AvailabilitySlotRequest {
+
+    @NotNull(message = "Start time is required")
+    private ZonedDateTime startTime;
+
+    @NotNull(message = "End time is required")
+    private ZonedDateTime endTime;
+
 }
