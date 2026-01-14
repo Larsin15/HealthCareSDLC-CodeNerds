5fb31b4e721fa9e336bf1050ec345935256a6680
Larsin15.Larsin15 <tommy.larsin@hotmail.com>
2026-01-14 12:25:18 +0100

    private variables for ResponseDTO aswell as default contructor for serialization

--- src/main/java/healthcareab/project/healthcare_booking_app/dto/AvailabilitySlotResponse.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/dto/AvailabilitySlotResponse.java
@@ -1,4 +1,35 @@
 package healthcareab.project.healthcare_booking_app.dto;
 
+import healthcareab.project.healthcare_booking_app.models.AvailabilitySlot;
+import healthcareab.project.healthcare_booking_app.models.SlotStatus;
+
 public class AvailabilitySlotResponse {
+
+    private UUID id;
+
+    private UUID employeeId;
+
+    private String employeeName; // "FirstName LastName"
+
+    private String employeeSpecialization; // Employee specialization ("Läkare", "SSK", "USK")
+
+    private ZonedDateTime startTime;
+
+    private ZonedDateTime endTime;
+
+    private SlotStatus status;
+
+    public AvailabilitySlotResponse() {
+    }
+
+    public AvailabilitySlotResponse(AvailabilitySlot slot) {
+        this.id = slot.getId();
+        this.employeeId = slot.getEmployee().getId();
+        this.employeeName = slot.getEmployee().getFirstName() + " " + slot.getEmployee().getLastName();
+        this.employeeSpecialization = slot.getEmployee().getSpecialization();
+        this.startTime = slot.getStartTime();
+        this.endTime = slot.getEndTime();
+        this.status = slot.getStatus();
+    }
 }
+
