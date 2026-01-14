c45a6c2e7e1dba3189430b98bf61994163ef2061
Larsin15.Larsin15 <tommy.larsin@hotmail.com>
2026-01-14 09:59:14 +0100

    The 4 enums added in SlotStatus.java

--- src/main/java/healthcareab/project/healthcare_booking_app/models/SlotStatus.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/models/SlotStatus.java
@@ -1,4 +1,12 @@
 package healthcareab.project.healthcare_booking_app.models;
 
 public enum SlotStatus {
+
+    AVAILABLE, // Available for patients to book
+
+    BOOKED, // Has been booked by a patient
+
+    CANCELLED, // Can be cancelled by employee (before booking) or by either party (after booking)
+
+    COMPLETED // Set automatically after the appointment end time passes
 }
