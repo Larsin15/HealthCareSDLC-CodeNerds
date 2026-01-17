fd80441942ef9f8856f30f5c8cc0368853f456e5
Larsin15.Larsin15 <tommy.larsin@hotmail.com>
2026-01-17 11:47:39 +0100

    variables and basic constructor for service

--- src/main/java/healthcareab/project/healthcare_booking_app/services/AppointmentService.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/services/AppointmentService.java
@@ -1,9 +1,25 @@
 package healthcareab.project.healthcare_booking_app.services;
 
+import healthcareab.project.healthcare_booking_app.repository.AppointmentRepository;
+import healthcareab.project.healthcare_booking_app.repository.AvailabilitySlotRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class AppointmentService {
+
+    private final AppointmentRepository appointmentRepository;
+    private final AvailabilitySlotRepository availabilitySlotRepository;
+
+    private static final int MIN_HOURS_BEFORE_BOOKING = 1;
+    private static final int MIN_HOURS_BEFORE_PATIENT_CANCEL = 24;
+
+    public AppointmentService(
+            AppointmentRepository appointmentRepository,
+            AvailabilitySlotRepository availabilitySlotRepository
+    ) {
+        this.appointmentRepository = appointmentRepository;
+        this.availabilitySlotRepository = availabilitySlotRepository;
+    }
 }
