a6a3383248ba02d0e36540d4aa59a09e7292c5c5
Larsin15.Larsin15 <tommy.larsin@hotmail.com>
2026-01-17 12:08:59 +0100

    Constructor for Appointment (patient view, no detailed patient information included)

--- src/main/java/healthcareab/project/healthcare_booking_app/dto/AppointmentResponse.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/dto/AppointmentResponse.java
@@ -1,6 +1,7 @@
 package healthcareab.project.healthcare_booking_app.dto;
 
 
+import healthcareab.project.healthcare_booking_app.models.Appointment;
 import healthcareab.project.healthcare_booking_app.models.AppointmentStatus;
 
 import java.time.LocalDateTime;
@@ -32,6 +33,33 @@ public class AppointmentResponse {
     private boolean canCancel;
 
     public AppointmentResponse() {
+    }
+
+    //Constructor from Appointment entity for patient view
+    public static AppointmentResponse forPatient(Appointment appointment) {
+        AppointmentResponse response = new AppointmentResponse();
+        response.id = appointment.getId();
+        response.availabilitySlotId = appointment.getAvailabilitySlot().getId();
+        response.slotStartTime = appointment.getAvailabilitySlot().getStartTime();
+        response.slotEndTime = appointment.getAvailabilitySlot().getEndTime();
+
+        response.employeeId = appointment.getEmployee().getId();
+        response.employeeName = appointment.getEmployee().getFirstName() + " "
+                + appointment.getEmployee().getLastName();
+        response.employeeSpecialization = appointment.getEmployee().getSpecialization();
+
+        response.patientId = appointment.getPatient().getId();
+        response.patientName = appointment.getPatient().getFirstName() + " "
+                + appointment.getPatient().getLastName();
+
+        response.status = appointment.getStatus();
+        response.notes = appointment.getNotes();
+        response.createdAt = appointment.getCreatedAt();
+        response.cancelledAt = appointment.getCancelledAt();
+
+        response.canCancel = appointment.canBeCancelledByPatient();
+
+        return response;
     }
 }
 
