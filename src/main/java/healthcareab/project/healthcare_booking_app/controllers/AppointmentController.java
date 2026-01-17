9275deccc58b865f5f5bcb3d77cbe265bfdaa0f4
Larsin15.Larsin15 <tommy.larsin@hotmail.com>
2026-01-17 12:22:59 +0100

    file AppointmentController added

---
+++ src/main/java/healthcareab/project/healthcare_booking_app/controllers/AppointmentController.java
@@ -0,0 +1,10 @@
+package healthcareab.project.healthcare_booking_app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/appointments")
+public class AppointmentController {
+}
+
