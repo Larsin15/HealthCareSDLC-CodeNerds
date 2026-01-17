74ca9b367c10644eef309f2471207ebae193ff09
Larsin15.Larsin15 <tommy.larsin@hotmail.com>
2026-01-17 12:07:06 +0100

    Variables with comments aswell as the empty constructor

--- src/main/java/healthcareab/project/healthcare_booking_app/dto/AppointmentResponse.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/dto/AppointmentResponse.java
@@ -1,6 +1,37 @@
 package healthcareab.project.healthcare_booking_app.dto;
 
 
+import healthcareab.project.healthcare_booking_app.models.AppointmentStatus;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
 public class AppointmentResponse {
+
+    private UUID id;
+    private UUID availabilitySlotId;
+    private ZonedDateTime slotStartTime;
+    private ZonedDateTime slotEndTime;
+
+    // Employee information
+    private UUID employeeId;
+    private String employeeName;
+    private String employeeSpecialization;
+
+    // Patient information
+    private UUID patientId;
+    private String patientName;
+
+    // Appointment information
+    private AppointmentStatus status;
+    private String notes;
+    private LocalDateTime createdAt;
+    private LocalDateTime cancelledAt;
+
+    private boolean canCancel;
+
+    public AppointmentResponse() {
+    }
 }
 
